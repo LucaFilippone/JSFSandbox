4d7917bf27a037733b4314084c13c2fe9a8b333a
LucaFilippone <contribute@172.25.13.228>
2013-10-02 12:27:05 +0200

    Renamed package Added template for dialogs Added dialoghandler to faces config Created personDialog

--- src/main/java/be/luca/sandbox/web/beans/CodiBean.java
+++ src/main/java/be/luca/sandbox/web/beans/CodiBean.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package be.luca.sandbox.web;
+package be.luca.sandbox.web.beans;
 
 import be.luca.sandbox.entity.Person;
 import be.luca.sandbox.service.PersonServiceBeanLocal;
--- src/main/java/be/luca/sandbox/web/beans/DialogFrameworkBean.java
+++ src/main/java/be/luca/sandbox/web/beans/DialogFrameworkBean.java
@@ -2,19 +2,22 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package be.luca.sandbox.web;
+package be.luca.sandbox.web.beans;
 
 import be.luca.sandbox.entity.Person;
 import be.luca.sandbox.service.PersonServiceBeanLocal;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 import org.omnifaces.util.Messages;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -38,8 +41,16 @@ public class DialogFrameworkBean implements Serializable{
     public void deletePerson(Person p){
         personServiceBean.deletePerson(p);
         Messages.addGlobalInfo("Person "+p.getFirstName()+" "+p.getName()+" has been removed");
-     
+        
         loadAllPersons();
+    }
+    
+    public void showDialog(){
+        Map<String,Object> options = new HashMap<String, Object>();  
+        options.put("modal", true); 
+        options.put("resizable",false);
+        
+        RequestContext.getCurrentInstance().openDialog("dialogs/personDialog",options,null);
     }
     
     private void loadAllPersons(){
